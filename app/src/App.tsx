f4db1e9ed6d3f44687f254bb1553f50e6c56a610
fistaszek2009 <150725634+fistaszek2009@users.noreply.github.com>
2026-03-22 23:02:20 +0100

    feat: generator structure

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,29 @@
 // import { useState } from 'react'
-import heroImg from './assets/hero.png'
 import './App.css'
+
 import Background from './components/Background'
 
 function App() {
 
   return (
-    <main>
+    <>
       <div id='app'>
-        cen
+        <h1>QRnerator</h1>
+        <main>
+          <nav>
+            <h3>Type ffff fff fff fff</h3>
+          </nav>
+          <div id='data'>
+            <h2>Title</h2>
+          </div>
+          <aside>
+            <h2>Code</h2>
+          </aside>
+          <div id='shadow'></div>
+        </main>
       </div>
       <Background/>
-    </main>
+    </>
   )
 }
 
