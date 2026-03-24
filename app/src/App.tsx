77a9c3f2179d478ffc0cc7a089fddde976c0e01b
fistaszek2009 <150725634+fistaszek2009@users.noreply.github.com>
2026-03-24 16:17:09 +0100

    feat: basic controls

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,21 +7,42 @@ function App() {
 
   return (
     <>
-      <div id='app'>
-        <h1>QRnerator</h1>
         <main>
+          <h1>QRnerator</h1>
           <nav>
-            <h3>Type ffff fff fff fff</h3>
+            <select name="type" id="type">
+              <option value="text">Text</option>
+              <option value="url">URL</option>
+              <option value="email">Email</option>
+              <option value="phone">Phone</option>
+              <option value="sms">SMS</option>
+              <option value="wifi">Wi-Fi</option>
+              <option value="vcard">vCard</option>
+            </select>
           </nav>
           <div id='data'>
             <h2>Title</h2>
           </div>
           <aside>
-            <h2>Code</h2>
+            <canvas id='code' width='200' height='200'></canvas>
+            <button id='download'>Download</button>
+            <div>
+              <label htmlFor='PNG'>
+                PNG <input type='radio' name='format' id='PNG' value='png' defaultChecked/>
+              </label>
+              <label htmlFor='JPG'>
+                JPG <input type='radio' name='format' id='JPG' value='jpg'/>
+              </label>
+              <label htmlFor='SVG'>
+                SVG <input type='radio' name='format' id='SVG' value='svg'/>
+              </label>
+              <label htmlFor='RAW'>
+                RAW <input type='radio' name='format' id='RAW' value='raw'/>
+              </label>
+            </div>
           </aside>
           <div id='shadow'></div>
         </main>
-      </div>
       <Background/>
     </>
   )
